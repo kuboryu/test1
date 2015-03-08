532f39029cf2590569807b075c8a59528704065f
kuboryu <kuboryu@macmini.local>
2015-03-08 17:34:49 +0900

    Initial Commit

---
+++ test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Created by kuboryu on 2015/03/08.
+//  Copyright (c) 2015年 kuboryu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test1
+//
+//  Created by kuboryu on 2015/03/08.
+//  Copyright (c) 2015年 kuboryu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
